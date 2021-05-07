a856fd870844f23b93858472ee1118022ae91354
jordanbertasso <36979824+jordanbertasso@users.noreply.github.com>
2021-05-07 22:44:08 +1000

    Update readme location for public repo

--- pages/api/commiters.js
+++ pages/api/commiters.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async (req, res) => {
-  const response = await fetch("https://raw.githubusercontent.com/MQU-MACS/first-commits/main/README.md?token=AI2EI4H7YQ4ZXCG7QJPH6VDASU2JE");
+  const response = await fetch("https://raw.githubusercontent.com/MQU-MACS/first-commits/main/README.md");
   const data = await response.text();
 
   if (!data) {
